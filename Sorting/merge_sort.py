774bdb2429369b592df7703c2354a49ffaf7a80e
Danielle McIntosh <118628680+invtrdan@users.noreply.github.com>
2023-04-09 21:00:37 -0400

    merge_sort()

--- Sorting/merge_sort.py
+++ Sorting/merge_sort.py
@@ -29,5 +29,11 @@ def merge(lst1, lst2):
             j += 1
     return res
 
-
+def merge_sort(lst):
+    if len(lst) <= 1:
+        return lst
+    mid = len(lst) // 2
+    left = merge_sort(lst[:mid])
+    right = merge_sort(lst[mid:])
+    return merge(left, right)
 
